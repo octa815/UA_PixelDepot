5ab3034dd148cd92453c1dc66dedb46cec9ee6ed
octa815 <ogg23@gcloud.ua.es>
2025-03-20 10:13:08 +0100

    Estructura del backend arreglada

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+mongoose.connect(process.env.MONGO_URI);
 const app = express();
 
 // Configuración
